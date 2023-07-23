d6b4a666f8ac7086edd79386474f67ae5adb65e4
muktasj07 <jmukta272@gmail.com>
2023-07-23 22:47:59 +0530

    Added total task code

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
-const inputBox=document.getElementById("input-box");
-const listContainer=document.getElementById("list-container");
+// JavaScript
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const totalTasksSpan = document.getElementById("total-tasks");
 
-
-
-function addTask(){
-if(inputBox.value===''){
-    alert("you must write something");
-}
-else{
-    let li=document.createElement("li");
-    li.innerHTML=inputBox.value;
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
-    let span=document.createElement("span");
-    span.innerHTML="\u00d7";
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
     li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+  updateTotalTasks();
 }
-inputBox.value="";
-saveData();
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+    updateTotalTasks();
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
 }
-listContainer.addEventListener("click",function(e){
-    if(e.target.tagName==='LI'){
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    else if(e.target.tagName==="SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-},
-    false);
 
-    function saveData(){
-        localStorage.setItem("data",listContainer.innerHTML);
-    }
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+  updateTotalTasks();
+}
 
+function updateTotalTasks() {
+  const tasks = listContainer.getElementsByTagName("li");
+  totalTasksSpan.textContent = tasks.length;
+}
 
-    function showTask(){
-listContainer.innerHTML=localStorage.getItem("data");
-    }
-    showTask();
+showTask();
